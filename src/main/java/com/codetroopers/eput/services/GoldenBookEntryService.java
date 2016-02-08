0fa3943e6a77ffd4b3b15a70c1078256b9cdfd21
Florian Chauveau <f.chauveau@code-troopers.com>
2016-02-08 18:37:06 +0100

    initial commit

---
+++ src/main/java/com/codetroopers/eput/services/GoldenBookEntryService.java
@@ -0,0 +1,30 @@
+package com.codetroopers.eput.services;
+
+import com.codetroopers.eput.domain.GoldenBookEntryDAO;
+import com.codetroopers.eput.domain.entities.GoldenBookEntry;
+import com.codetroopers.eput.models.UserInfo;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import java.util.List;
+
+/**
+ * This class is annotated with the Stateless marker. It allows to automatically handle transactions.
+ */
+@Stateless
+public class GoldenBookEntryService {
+    @Inject
+    UserInfo userInfo;
+    @Inject
+    GoldenBookEntryDAO bookEntryDAO;
+
+
+    public void insertNewGoldenBookEntry(final GoldenBookEntry entry) {
+
+
+    }
+
+    public List<GoldenBookEntry> loadGoldenBookEntries() {
+        return bookEntryDAO.all();
+    }
+}
